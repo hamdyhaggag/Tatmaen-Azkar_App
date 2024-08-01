3740b88d538c55aef1b0324529b4042744ce7298
hamdy haggag <hamdyhaggag74@gmail.com>
2024-08-01 20:30:50 +0300

    Refactor Azkar Items Screen: Improve Readability and Modularize Code

--- lib/Presentation/screens/azkar_list_screen.dart
+++ lib/Presentation/screens/azkar_list_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
-
 import 'package:tatmaen24/Presentation/screens/add_azkar_item.dart';
 import 'package:tatmaen24/imports.dart';
 
@@ -39,61 +38,59 @@ class AzkarItem {
 
 Future<void> loadAzkarItems() async {
   final prefs = await SharedPreferences.getInstance();
-  final String? azkarJson = prefs.getString('azkar_items');
+  final azkarJson = prefs.getString('azkar_items');
 
   if (azkarJson != null) {
     final List<dynamic> azkarList = json.decode(azkarJson);
-    final List<AzkarItem> azkarItems =
+    azkarNotifier.value =
         azkarList.map((item) => AzkarItem.fromJson(item)).toList();
-    azkarNotifier.value = azkarItems;
   } else {
-    azkarNotifier.value = [
-      AzkarItem(
-        text: 'سبحان الله',
-        count: 33,
-        reward: 'لها ثواب عظيم عند الله',
-        isDefault: true,
-      ),
-      AzkarItem(
-        text: 'الحمد لله',
-        count: 33,
-        reward: 'لك أجر عظيم عند الله وذلك لأنك حمدته على نعمه',
-        isDefault: true,
-      ),
-      AzkarItem(
-        text: 'الله أكبر',
-        count: 100,
-        reward: 'لا يعلم أجرها إلا الله',
-        isDefault: true,
-      ),
-      AzkarItem(
-        text: 'لا إله إلا الله',
-        count: 33,
-        reward: 'لها فضل عظيم لا يعلمه إلا الله',
-        isDefault: true,
-      ),
-      AzkarItem(
-        text: 'لا حول ولا قوة إلا بالله',
-        count: 33,
-        reward: 'لك على كل تسبيحه شجرة بالجنه',
-        isDefault: true,
-      ),
-      AzkarItem(
-        text: 'اللهم صلِّ على سيدنا محمد',
-        count: 100,
-        reward: 'يرد عليك الرسول صلى الله عليه وسلم السلام',
-        isDefault: true,
-      ),
-    ];
-    await saveAzkarItems();
+    _setDefaultAzkarItems();
   }
 }
 
+void _setDefaultAzkarItems() async {
+  azkarNotifier.value = _defaultAzkarItems();
+  await saveAzkarItems();
+}
+
+List<AzkarItem> _defaultAzkarItems() => [
+      AzkarItem(
+          text: 'سبحان الله',
+          count: 33,
+          reward: 'لها ثواب عظيم عند الله',
+          isDefault: true),
+      AzkarItem(
+          text: 'الحمد لله',
+          count: 33,
+          reward: 'لك أجر عظيم عند الله وذلك لأنك حمدته على نعمه',
+          isDefault: true),
+      AzkarItem(
+          text: 'الله أكبر',
+          count: 100,
+          reward: 'لا يعلم أجرها إلا الله',
+          isDefault: true),
+      AzkarItem(
+          text: 'لا إله إلا الله',
+          count: 33,
+          reward: 'لها فضل عظيم لا يعلمه إلا الله',
+          isDefault: true),
+      AzkarItem(
+          text: 'لا حول ولا قوة إلا بالله',
+          count: 33,
+          reward: 'لك على كل تسبيحه شجرة بالجنه',
+          isDefault: true),
+      AzkarItem(
+          text: 'اللهم صلِّ على سيدنا محمد',
+          count: 100,
+          reward: 'يرد عليك الرسول صلى الله عليه وسلم السلام',
+          isDefault: true),
+    ];
+
 Future<void> saveAzkarItems() async {
   final prefs = await SharedPreferences.getInstance();
-  final List<Map<String, dynamic>> azkarList =
-      azkarNotifier.value.map((item) => item.toJson()).toList();
-  final String azkarJson = json.encode(azkarList);
+  final azkarJson =
+      json.encode(azkarNotifier.value.map((item) => item.toJson()).toList());
   await prefs.setString('azkar_items', azkarJson);
 }
 
@@ -104,15 +101,11 @@ class AzkarListScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final isDarkMode = Theme.of(context).brightness == Brightness.dark;
 
-    // Load azkar items when the screen is built
-    loadAzkarItems();
+    loadAzkarItems(); // Fetch data when screen builds
 
     return Scaffold(
       backgroundColor: isDarkMode ? const Color(0xff1F1F1F) : Colors.white,
-      appBar: const CustomAppBar(
-        title: 'السبحة الإلكترونية',
-        isHome: true,
-      ),
+      appBar: const CustomAppBar(title: 'السبحة الإلكترونية', isHome: true),
       body: ValueListenableBuilder<List<AzkarItem>>(
         valueListenable: azkarNotifier,
         builder: (context, azkar, child) {
@@ -120,81 +113,8 @@ class AzkarListScreen extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             itemCount: azkar.length,
             itemBuilder: (context, index) {
-              return Dismissible(
-                key: Key(azkar[index].text),
-                background: Container(color: Colors.red),
-                confirmDismiss: (direction) async {
-                  if (azkar[index].isDefault) {
-                    return false;
-                  }
-                  if (direction == DismissDirection.startToEnd) {
-                    return false;
-                  }
-                  return await showDialog<bool>(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                      title: Text(
-                        'تأكيد الحذف',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontFamily: 'DIN',
-                            fontWeight: FontWeight.w600,
-                            fontSize: 20.sp,
-                            color: isDarkMode
-                                ? Colors.white
-                                : AppColors.primaryColor),
-                      ),
-                      content: Text(
-                        textAlign: TextAlign.center,
-                        'هل أنت متأكد أنك تريد حذف هذا الذكر؟',
-                        style: TextStyle(
-                            fontFamily: 'DIN',
-                            fontSize: 22.sp,
-                            color: isDarkMode
-                                ? Colors.white
-                                : AppColors.primaryColor),
-                      ),
-                      actions: [
-                        TextButton(
-                          onPressed: () => Navigator.pop(context, false),
-                          child: Text(
-                            'إلغاء',
-                            style: TextStyle(
-                                fontFamily: 'DIN',
-                                fontSize: 16.sp,
-                                color: isDarkMode
-                                    ? Colors.white
-                                    : AppColors.primaryColor),
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () => Navigator.pop(context, true),
-                          child: Text(
-                            'حذف',
-                            style: TextStyle(
-                                fontFamily: 'DIN',
-                                fontSize: 16.sp,
-                                color: isDarkMode
-                                    ? Colors.white
-                                    : AppColors.primaryColor),
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-                onDismissed: (direction) {
-                  if (!azkar[index].isDefault) {
-                    azkarNotifier.value = List.from(azkarNotifier.value)
-                      ..removeAt(index);
-                    saveAzkarItems();
-                  }
-                },
-                child: AzkarListItem(
-                  azkarItem: azkar[index],
-                  onDelete: () {},
-                ),
-              );
+              final item = azkar[index];
+              return _buildAzkarItem(context, item, index, isDarkMode);
             },
           );
         },
@@ -205,9 +125,7 @@ class AzkarListScreen extends StatelessWidget {
           onPressed: () {
             Navigator.push(
               context,
-              MaterialPageRoute(
-                builder: (context) => const AddAzkarScreen(),
-              ),
+              MaterialPageRoute(builder: (context) => const AddAzkarScreen()),
             );
             Vibrate.feedback(FeedbackType.heavy);
           },
@@ -216,14 +134,69 @@ class AzkarListScreen extends StatelessWidget {
       ],
     );
   }
+
+  Widget _buildAzkarItem(
+      BuildContext context, AzkarItem item, int index, bool isDarkMode) {
+    return Dismissible(
+      key: Key(item.text),
+      background: Container(color: Colors.red),
+      confirmDismiss: (direction) =>
+          _confirmDismiss(context, item, direction, isDarkMode),
+      onDismissed: (direction) {
+        if (!item.isDefault) {
+          azkarNotifier.value = List.from(azkarNotifier.value)..removeAt(index);
+          saveAzkarItems();
+        }
+      },
+      child: AzkarListItem(azkarItem: item),
+    );
+  }
+
+  Future<bool?> _confirmDismiss(BuildContext context, AzkarItem item,
+      DismissDirection direction, bool isDarkMode) {
+    if (item.isDefault || direction != DismissDirection.endToStart)
+      return Future.value(false);
+
+    return showDialog<bool>(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(
+          'تأكيد الحذف',
+          textAlign: TextAlign.center,
+          style: _dialogTextStyle(isDarkMode),
+        ),
+        content: Text(
+          'هل أنت متأكد أنك تريد حذف هذا الذكر؟',
+          textAlign: TextAlign.center,
+          style: _dialogTextStyle(isDarkMode),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context, false),
+            child: Text('إلغاء', style: _dialogTextStyle(isDarkMode)),
+          ),
+          TextButton(
+            onPressed: () => Navigator.pop(context, true),
+            child: Text('حذف', style: _dialogTextStyle(isDarkMode)),
+          ),
+        ],
+      ),
+    );
+  }
+
+  TextStyle _dialogTextStyle(bool isDarkMode) {
+    return TextStyle(
+      fontFamily: 'DIN',
+      fontSize: 16.sp,
+      color: isDarkMode ? Colors.white : AppColors.primaryColor,
+    );
+  }
 }
 
 class AzkarListItem extends StatelessWidget {
   final AzkarItem azkarItem;
-  final VoidCallback onDelete;
 
-  const AzkarListItem(
-      {super.key, required this.azkarItem, required this.onDelete});
+  const AzkarListItem({super.key, required this.azkarItem});
 
   @override
   Widget build(BuildContext context) {
@@ -237,44 +210,15 @@ class AzkarListItem extends StatelessWidget {
         title: Text(
           azkarItem.text,
           textAlign: TextAlign.right,
-          style: TextStyle(
-              fontSize: 21.sp,
-              fontFamily: 'DIN',
-              color: isDarkMode ? Colors.white : AppColors.primaryColor,
-              fontWeight: FontWeight.w600),
+          style: _textStyle(isDarkMode,
+              fontSize: 21.sp, fontWeight: FontWeight.w600),
         ),
         subtitle: Text(
           azkarItem.reward,
           textAlign: TextAlign.right,
-          style: TextStyle(
-              fontSize: 16.sp,
-              fontFamily: 'DIN',
-              color: isDarkMode ? Colors.white : AppColors.primaryColor),
+          style: _textStyle(isDarkMode, fontSize: 16.sp),
         ),
-        trailing: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              '${azkarItem.count}',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                  fontSize: 22.sp,
-                  fontFamily: 'DIN',
-                  color: isDarkMode ? Colors.white : AppColors.primaryColor),
-            ),
-            SizedBox(
-              width: 26.w,
-            ),
-            Text(
-              'مرة',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                  fontSize: 18.sp,
-                  fontFamily: 'DIN',
-                  color: isDarkMode ? Colors.white : AppColors.primaryColor),
-            ),
-          ],
-        ),
+        trailing: _buildTrailing(isDarkMode),
         onTap: () {
           Navigator.push(
             context,
@@ -288,6 +232,35 @@ class AzkarListItem extends StatelessWidget {
           );
         },
       ),
+    );
+  }
+
+  Widget _buildTrailing(bool isDarkMode) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          '${azkarItem.count}',
+          textAlign: TextAlign.left,
+          style: _textStyle(isDarkMode, fontSize: 22.sp),
+        ),
+        SizedBox(width: 26.w),
+        Text(
+          'مرة',
+          textAlign: TextAlign.left,
+          style: _textStyle(isDarkMode, fontSize: 18.sp),
+        ),
+      ],
+    );
+  }
+
+  TextStyle _textStyle(bool isDarkMode,
+      {required double fontSize, FontWeight fontWeight = FontWeight.normal}) {
+    return TextStyle(
+      fontSize: fontSize,
+      fontFamily: 'DIN',
+      color: isDarkMode ? Colors.white : AppColors.primaryColor,
+      fontWeight: fontWeight,
     );
   }
 }
